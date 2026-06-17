1bc65790a8d347bc8c85d836bbe1558c10eba651
Thirumala Prasad <stprasdmtech@gmail.com>
2026-06-17 14:21:42 +0530

    committed the code

---
+++ src/main/java/com/thirumala/work/KubeDeploymentTestApplication.java
@@ -0,0 +1,13 @@
+package com.thirumala.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeDeploymentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeDeploymentTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/thirumala/work/controller/AppController.java
@@ -0,0 +1,26 @@
+package com.thirumala.work.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AppController {
+	
+	@GetMapping
+	public String welcome() {
+		return "Welcome to the Kubernetes Deployment Test application...";
+	}
+
+	@GetMapping("/hi")
+	public String sayHi() {
+		return "Hi from the application";
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello from the application";
+	}
+	
+	@GetMapping("/bye")
+	public String sayBye() {
+		return "Bye from the application";
+	}
+}
